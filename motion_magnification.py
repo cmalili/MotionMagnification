541932d5d7209a6c26e0be1fe70403d5a9db0fc1
Christopher, Malili <malilichristopher@gmail.com>
2025-02-26 19:40:51 -0800

    Created motion_magnification file with questions

---
+++ motion_magnification.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 26 19:34:26 2025
+
+@author: cmalili
+"""
+#part a
+
+'''
+Read the original paper on Eulerian video magnification https://people.csail.mit.edu/
+mrub/papers/vidmag.pdf. 
+Write a code to reproduce Figure 4, amplifying a 
+ (1) a simple sinusoid as done in the figure; and 
+ (2) pick an EEG or heart rate signal or equivalent physiological signal and amplify certain frequencies inside of it. For (2), note where you
+got the signal (cite the source), and 
+explain which set of frequencies you chose to amplify and why (i.e. their physiological relevance). (10 points)
+'''
+
+
+
+#Part b
+'''
+Take a video of your face with very little movement (try to be as still as possible!). Then
+implement a version of Eulerian video magnification to amplify the color in your face due
+to blood pulse. If you are having difficulty with amplifying the color in your face, document
+your challenges (i.e. skin tone, etc) and find an alternative video that you capture to
+magnify. Also, it would be good to try and capture RAW data if possible, using either a
+DSLR camera or download an application to extract it from your cellphone, as that will
+lend you better results. Document your method’s implementation and your results on your
+data.
+'''
+
+
+#GTK
+'''
+Note: the existing code implementations here https://people.csail.mit.edu/mrub/
+vidmag/#code may be useful to refer as you implement your method. It’s highly rec-
+ommended to get your method working on their data first, then try to get it working on
+your own data.
+'''
